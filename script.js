738c4ae24ec3054472a150afbe331673c59b7351
Mphan87 <102190333+Mphan87@users.noreply.github.com>
2022-03-31 08:37:17 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,39 @@
 const gameContainer = document.getElementById("game");
 let card1 = null;
 let card2 = null;
+let card3 = null;
 let Score = 0;
 let noClicking = false;
 
 const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
   "black",
   "black",
+  "blue",
+  "blue",
   "brown",
   "brown",
+  "green",
+  "green",
+  "orange",
+  "orange",
+  "purple",
+  "purple",
+  "red",
+  "red",
   "Teal",
   "Teal",
   "turquoise",
   "turquoise",
+  "black",
+  "blue",
+  "brown",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "Teal",
+  "turquoise",
+  
 
 ];
 
@@ -66,7 +77,8 @@ function handleCardClick(e) {
 
   console.dir(e.target)
 
-  if (!card1 || !card2)
+
+  if (!card1 || !card2 ||!card3)
     currentCard.classList.add("flipped");
 
   if (card1 === null)
@@ -78,27 +90,38 @@ function handleCardClick(e) {
     card2 === null
   }
 
-  if (card1 && card2) {
+  if (currentCard != card1 && currentCard != card2 && card1 != null && card2 != null && card3 === null) {
+    card3 = currentCard;
+  } else {
+    card3 === null
+  }
+
+  if (card1 && card2 && card3) {
     noClicking = true;
     let flippedCard1 = card1.id;
     let flippedCard2 = card2.id;
+    let flippedCard3 = card3.id;
 
-
-    if (flippedCard1 === flippedCard2) {
-      Score += 2;
+    if (flippedCard1 === flippedCard2 & flippedCard1 === flippedCard3) {
+      Score += 3;
       card1.removeEventListener("click", handleCardClick);
       card2.removeEventListener("click", handleCardClick);
+      card3.removeEventListener("click", handleCardClick);
       card1 = null;
       card2 = null;
+      card3 = null;
       noClicking = false;
     } else {
       setTimeout(function () {
         card1.style.backgroundColor = "";
         card2.style.backgroundColor = "";
+        card3.style.backgroundColor = "";
         card1.classList.remove("flipped");
         card2.classList.remove("flipped");
+        card3.classList.remove("flipped");
         card1 = null;
         card2 = null;
+        card3 = null;
         noClicking = false;
       }, 1000);
     }
